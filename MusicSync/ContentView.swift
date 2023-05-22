a58f955362b9db9d2088081ad15297c6e6db1714
NobuyaTagawa <090.sket@gmail.com>
2023-05-22 11:26:19 +0900

    Initial Commit

---
+++ MusicSync/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MusicSync
+//
+//  Created by 田川展也 on R 5/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MusicSync/MusicSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicSyncApp.swift
+//  MusicSync
+//
+//  Created by 田川展也 on R 5/05/22.
+//
+
+import SwiftUI
+
+@main
+struct MusicSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
