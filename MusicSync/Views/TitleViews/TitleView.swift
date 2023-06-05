33de6ca7a99e58e279798045a200bc74aa3b09d1
NobuyaTagawa <090.sket@gmail.com>
2023-06-05 15:18:51 +0900

    Add test code in TitleView

--- MusicSync/Views/TitleViews/TitleView.swift
+++ MusicSync/Views/TitleViews/TitleView.swift
@@ -10,6 +10,9 @@ import MusicKit
 
 struct TitleView: View {
     
+    //テスト
+    var model = FirestoreModel()
+    
     @EnvironmentObject var transData: EnvironmentData
     
     @State var appleAuthStatus: MusicAuthorization.Status
@@ -39,7 +42,41 @@ struct TitleView: View {
                         .foregroundColor(Color.white)
                         .scaledToFit()
                         .padding(50)
-                    
+                    //テスト
+                    Button("プレイリスト作成"){
+                        model.downloadData(roomPin: 613901, usersData: [UserData(id: "29048C80-15EE-401B-8F9B-F661B2EA4C54", name: "さいと"), UserData(id: "A4700E05-9D3A-44E2-AE0F-04B99B1E583F" , name: "なつみかん")]) { result in
+                            do{
+                                let data = try result.get()
+                                print("data[0]")
+                                print(data[0])
+                                 
+                                print("data[1]")
+                                print(data[1])
+                                
+                                Task{
+                                    do{
+                                        try await MusicLibrary.shared.createPlaylist(name: "1st Playlist", items: data[0] )
+                                        print("1st finish")
+                                    }catch{
+                                        print("error: \(error)")
+                                    }
+                                    
+                                    do{
+                                        try await MusicLibrary.shared.createPlaylist(name: "2nd Playlist", items: data[1] )
+                                        print("2nd finish")
+                                    }catch{
+                                        print("error: \(error)")
+                                    }
+                                }
+                                
+                                
+                                
+                            } catch {
+                                print("error: \(error)")
+                            }
+                        }
+                    }
+                    //終わり
                     
                     NavigationLink(destination: LogInView(isTitleViewActive: $isActive),isActive: $isActive ,
                                    label: {Button(action: {
@@ -70,13 +107,9 @@ struct TitleView: View {
                     AppleMusicAuthView(appleAuthStatus: $appleAuthStatus)
                         .scaleEffect(appleAuthStatus != .authorized ? 1 : 0)
                         .animation(.easeIn, value: appleAuthStatus != .authorized)
-//                        .onAppear(){
-//                            isAnimation = true
-//                        }
+                    
+                    
                 }
-                    
-                    
-                
             }
         }
     }
