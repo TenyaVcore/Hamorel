aba451dad01a1060a4cb3b09659f80a44edbd94b
NobuyaTagawa <090.sket@gmail.com>
2023-11-02 14:16:03 +0900

    fix: Recreate HomeView and replace navigationStack

---
+++ MusicSync/Enums/NavigationLinkItemNavigationItem.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationItem.swift
+//  MusicSync
+//
+//  Created by 田川展也 on R 5/11/02.
+//
+
+import Foundation
+
+enum NavigationLinkItem: Hashable{
+    case home
+    case create
+    case join(Int)
+    case enter
+    case setting
+    case login
+}
