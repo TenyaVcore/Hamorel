40ee0f187d52c64aee17911949db5ac051aa362b
NobuyaTagawa <090.sket@gmail.com>
2023-11-02 14:32:54 +0900

    feat: Create MemberListCell

---
+++ MusicSync/Views/ViewComponents/MemberListCell.swift
@@ -0,0 +1,18 @@
+//
+//  MemberListCell.swift
+//  MusicSync
+//
+//  Created by 田川展也 on R 5/11/02.
+//
+
+import SwiftUI
+
+struct MemberListCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MemberListCell()
+}
