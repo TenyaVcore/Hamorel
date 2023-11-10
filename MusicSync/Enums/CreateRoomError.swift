94f47d40e0049e7b17cfaa7d8fd094f91e7f8a43
NobuyaTagawa <090.sket@gmail.com>
2023-11-10 12:27:53 +0900

    feat: Update CreateRoomError contents.

--- MusicSync/Enums/CreateRoomError.swift
+++ MusicSync/Enums/CreateRoomError.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+enum CreateRoomError: Error {
+    case canNotFindRoomPin10Times
+    case otherError(Error)
+    
+    var localizedDescription: String {
+        switch self {
+        case .canNotFindRoomPin10Times: return "room Pinの作成に10回失敗しました"
+        case .otherError(let error): return "予期せぬエラーが発生しました。詳細: （\(error)）"
+            
+        }
+    }
+}
