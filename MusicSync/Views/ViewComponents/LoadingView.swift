1ca86fcbccd773ed3a95a206a4ef1f611be176ce
NobuyaTagawa <090.sket@gmail.com>
2023-11-10 12:26:41 +0900

    feat: Update LoadingView design.

--- MusicSync/Views/ViewComponents/LoadingView.swift
+++ MusicSync/Views/ViewComponents/LoadingView.swift
@@ -8,38 +8,55 @@
 import SwiftUI
 
 struct LoadingView: View {
+    @State private var isAnimating = false
+    @State private var numberOfDots = 0
+        
+    let maxDots = 4
+    var message: String
+    
     var body: some View {
         ZStack{
-            Color(.black)
-                .opacity(0.1)
-                .edgesIgnoringSafeArea(.all)
-                .disabled(true)
-            
-            ProgressView("Now loading...")
-                .padding(80)
-                .background(Color(uiColor: .lightGray))
-                .cornerRadius(12)
+            Color("Color_primary")
+                .ignoresSafeArea()
+            VStack{
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .rotationEffect(.degrees(isAnimating ? 360 : 0))
+                    .animation(
+                        .linear(duration: 3).repeatForever(autoreverses: false),
+                        value: isAnimating
+                    )
+                
+                HStack(spacing: 0) {
+                    Text(message)
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundStyle(.white)
+                    
+                    ForEach(0 ..< numberOfDots, id: \.self) { _ in
+                        Text(".")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .foregroundStyle(.white)
+                    }
+                }
+                .onAppear {
+                    isAnimating = true
+                    _ = withAnimation(Animation.easeInOut(duration: 3).repeatForever(autoreverses: true)) {
+                        Timer.scheduledTimer(withTimeInterval: 3 / Double(maxDots), repeats: true) { timer in
+                                    numberOfDots = (numberOfDots + 1) % (maxDots + 1)
+                                }
+                            }
+                }
+            }
         }
     }
-}
-
-
-class LoadingControl: ObservableObject {
-    static let shared = LoadingControl()
-    
-    @Published var isLoading = false
-    
-    func showLoading(){
-        isLoading = true
-    }
-    
-    func hideLoading() {
-        isLoading = false
-    }
+        
 }
 
 struct loadingView_Previews: PreviewProvider {
     static var previews: some View {
-        LoadingView()
+        LoadingView(message: "ローディング中")
     }
 }
