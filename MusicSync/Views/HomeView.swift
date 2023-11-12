689a537698b2b73934b805b10856692d459123cf
NobuyaTagawa <090.sket@gmail.com>
2023-11-12 21:22:29 +0900

    feat: Present AppleMusicAuthView when status is not authorized.

--- MusicSync/Views/HomeView.swift
+++ MusicSync/Views/HomeView.swift
@@ -26,93 +26,99 @@ struct HomeView: View {
         let bounds = UIScreen.main.bounds
         let screenHeight = Int(bounds.height)
 
-        NavigationStack(path: $path) {
-            VStack {
-                Spacer()
+        ZStack {
+            NavigationStack(path: $path) {
+                VStack {
+                    Spacer()
 
-                Image("MusicSync_logo")
-                    .resizable()
-                    .scaledToFit()
+                    Image("MusicSync_logo")
+                        .resizable()
+                        .scaledToFit()
 
-                Spacer()
+                    Spacer()
 
-                ZStack {
-                    RoundedCorners(color: .white, tl: 20, tr: 20, bl: 0, br: 0)
-                        .ignoresSafeArea()
-                        .frame(height: (CGFloat(screenHeight) / 1.8))
+                    ZStack {
+                        RoundedCorners(color: .white, tl: 20, tr: 20, bl: 0, br: 0)
+                            .ignoresSafeArea()
+                            .frame(height: (CGFloat(screenHeight) / 1.8))
 
-                    VStack {
-                        Text("ようこそ\(name)さん")
-                            .font(.title3)
-                            .fontWeight(.bold)
-                            .padding(.top, 10)
-
-                        Text("友人家族と音楽で繋がろう")
-                            .font(.title3)
-                            .fontWeight(.bold)
-                            .padding(.bottom, 30)
-
-                        NavigationLink(value: NavigationLinkItem.create) {
-                            ButtonView(text: "ルームを作成する", textColor: .white, buttonColor: Color("Color_primary"))
-                        }
-                        .padding(.bottom, 20)
-
-                        NavigationLink(value: NavigationLinkItem.enter) {
-                            ButtonView(text: "ルームに参加する", textColor: .black, buttonColor: Color("Color_secondary"))
-                        }
-                        .padding(.bottom, 10)
-
-                        Divider()
-
-                        NavigationLink(value: NavigationLinkItem.login) {
-                            Text("ログイン")
+                        VStack {
+                            Text("ようこそ\(name)さん")
                                 .font(.title3)
                                 .fontWeight(.bold)
-                                .foregroundColor(.black)
-                                .frame(width: 300, height: 50)
-                                .background(Color("Color_secondary"))
-                                .cornerRadius(10)
+                                .padding(.top, 10)
+
+                            Text("友人家族と音楽で繋がろう")
+                                .font(.title3)
+                                .fontWeight(.bold)
+                                .padding(.bottom, 30)
+
+                            NavigationLink(value: NavigationLinkItem.create) {
+                                ButtonView(text: "ルームを作成する", textColor: .white, buttonColor: Color("Color_primary"))
+                            }
+                            .padding(.bottom, 20)
+
+                            NavigationLink(value: NavigationLinkItem.enter) {
+                                ButtonView(text: "ルームに参加する", textColor: .black, buttonColor: Color("Color_secondary"))
+                            }
+                            .padding(.bottom, 10)
+
+                            Divider()
+
+                            NavigationLink(value: NavigationLinkItem.login) {
+                                Text("ログイン")
+                                    .font(.title3)
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.black)
+                                    .frame(width: 300, height: 50)
+                                    .background(Color("Color_secondary"))
+                                    .cornerRadius(10)
+                            }
+                            .padding(.vertical, 10)
                         }
-                        .padding(.vertical, 10)
+                    }
+                }
+                .background {
+                    Color("Color_primary")
+                        .ignoresSafeArea()
+                }
+                .toolbar {
+                    ToolbarItem(placement: .topBarLeading) {
+                        NavigationLink(value: NavigationLinkItem.setting) {
+                            Image(systemName: "gearshape.fill")
+                                .foregroundColor(.white)
+                                .font(.title)
+                        }
+                    }
+                }
+                .navigationDestination(for: NavigationLinkItem.self) { item in
+                    switch item {
+                    case .create:
+                        CreateGroupView(path: $path, name: name)
+                    case .enter:
+                        EnterRoomPinView(path: $path)
+                    case .join(let roomPin):
+                        JoinGroupView(path: $path, name: name, roomPin: roomPin)
+                    case .playlist(let roomPin):
+                        CreatePlaylistView(roomPin: roomPin, usersData: [])
+                    case .home:
+                        HomeView()
+                    case .setting:
+                        SettingView()
+                    case .login:
+                        LogInView(path: $path)
+                    case .passwordReset:
+                        PasswordResetView(path: $path)
+                    case .register:
+                        EmailRegisterView(path: $path)
+                    case .provision:
+                        ProvisionalRegistrationView(path: $path)
                     }
                 }
             }
-            .background {
-                Color("Color_primary")
-                    .ignoresSafeArea()
-            }
-            .toolbar {
-                ToolbarItem(placement: .topBarLeading) {
-                    NavigationLink(value: NavigationLinkItem.setting) {
-                        Image(systemName: "gearshape.fill")
-                            .foregroundColor(.white)
-                            .font(.title)
-                    }
-                }
-            }
-            .navigationDestination(for: NavigationLinkItem.self) { item in
-                switch item {
-                case .create:
-                    CreateGroupView(path: $path, name: name)
-                case .enter:
-                    EnterRoomPinView(path: $path)
-                case .join(let roomPin):
-                    JoinGroupView(path: $path, name: name, roomPin: roomPin)
-                case .playlist(let roomPin):
-                    CreatePlaylistView(roomPin: roomPin, usersData: [])
-                case .home:
-                    HomeView()
-                case .setting:
-                    SettingView()
-                case .login:
-                    LogInView(path: $path)
-                case .passwordReset:
-                    PasswordResetView(path: $path)
-                case .register:
-                    EmailRegisterView(path: $path)
-                case .provision:
-                    ProvisionalRegistrationView(path: $path)
-                }
+
+            if appleAuthStatus != .authorized {
+                AppleMusicAuthView(appleAuthStatus: $appleAuthStatus)
             }
         }
     }
