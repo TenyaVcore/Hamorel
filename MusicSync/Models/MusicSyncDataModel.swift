7ac478a6779e819358712b5c2666f3e3fa4a1aa7
NobuyaTagawa <090.sket@gmail.com>
2024-08-02 14:10:49 +0900

    feat: MusicSyncDataModelを定義

---
+++ MusicSync/Models/MusicSyncDataModel.swift
@@ -0,0 +1,13 @@
+//
+//  MusicSyncData.swift
+//  MusicSync
+//
+//  Created by 田川展也 on R 6/08/02.
+//
+
+
+struct MusicSyncDataModel: Codable {
+    var title: String
+    var artistName: String
+    var appleMusicID: String?
+}
